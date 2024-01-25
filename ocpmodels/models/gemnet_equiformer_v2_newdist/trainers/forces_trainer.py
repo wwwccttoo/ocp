3aa263c512ce1d6b400d19461199a97dd3a8c2f8
wwwccttoo <ketong_shao@berkeley.edu>
2024-01-25 12:46:59 -0800

    reduce memory

--- ocpmodels/models/gemnet_equiformer_v2_newdist/trainers/forces_trainer.py
+++ ocpmodels/models/gemnet_equiformer_v2_newdist/trainers/forces_trainer.py
@@ -14,6 +14,7 @@ import torch
 import torch.optim as optim
 import torch_geometric
 from torch.nn.parallel.distributed import DistributedDataParallel
+from torch.utils.checkpoint import checkpoint
 from tqdm import tqdm
 
 from ocpmodels.common import distutils
@@ -116,22 +117,42 @@ class GemnetEquiformerV2ForcesTrainer(ForcesTrainer):
             )
 
     def load_optimizer(self):
-        optimizer = self.config["optim"].get("optimizer", "AdamW")
-        optimizer = getattr(optim, optimizer)
-        optimizer_params = self.config["optim"]["optimizer_params"]
-        weight_decay = optimizer_params["weight_decay"]
+        if self.config["optim"].get("quantization", False):
+            import bitsandbytes as bnb
 
-        parameters, name_no_wd = add_weight_decay(
-            self.model, weight_decay, self.model_params_no_wd
-        )
-        logging.info("Parameters without weight decay:")
-        logging.info(name_no_wd)
+            optimizer = self.config["optim"].get("optimizer", "AdamW")
+            optimizer = getattr(bnb.optim, optimizer)
+            optimizer_params = self.config["optim"]["optimizer_params"]
+            weight_decay = optimizer_params["weight_decay"]
 
-        self.optimizer = optimizer(
-            parameters,
-            lr=self.config["optim"]["lr_initial"],
-            **optimizer_params,
-        )
+            parameters, name_no_wd = add_weight_decay(
+                self.model, weight_decay, self.model_params_no_wd
+            )
+            logging.info("Parameters without weight decay:")
+            logging.info(name_no_wd)
+
+            self.optimizer = optimizer(
+                parameters,
+                lr=self.config["optim"]["lr_initial"],
+                **optimizer_params,
+            )
+        else:
+            optimizer = self.config["optim"].get("optimizer", "AdamW")
+            optimizer = getattr(optim, optimizer)
+            optimizer_params = self.config["optim"]["optimizer_params"]
+            weight_decay = optimizer_params["weight_decay"]
+
+            parameters, name_no_wd = add_weight_decay(
+                self.model, weight_decay, self.model_params_no_wd
+            )
+            logging.info("Parameters without weight decay:")
+            logging.info(name_no_wd)
+
+            self.optimizer = optimizer(
+                parameters,
+                lr=self.config["optim"]["lr_initial"],
+                **optimizer_params,
+            )
 
     def load_extras(self):
         def multiply(obj, num):
@@ -318,7 +339,10 @@ class GemnetEquiformerV2ForcesTrainer(ForcesTrainer):
 
     def _forward(self, batch_list):
         # forward pass.
-        gemnet_out, equiformer_out = self.model(batch_list)
+        if self.config["optim"].get("gradient_checkpoint", False):
+            gemnet_out, equiformer_out = checkpoint(self.model, batch_list)
+        else:
+            gemnet_out, equiformer_out = self.model(batch_list)
         if self.config["model_attributes"].get("regress_forces", True):
             gemnet_out_energy, gemnet_out_forces = gemnet_out
             equiformer_energy, equiformer_forces = equiformer_out
@@ -793,3 +817,196 @@ class GemnetEquiformerV2ForcesTrainer(ForcesTrainer):
         )
 
         return out
+
+    def train(self, disable_eval_tqdm: bool = False) -> None:
+        ensure_fitted(self._unwrapped_model, warn=True)
+
+        eval_every = self.config["optim"].get(
+            "eval_every", len(self.train_loader)
+        )
+        checkpoint_every = self.config["optim"].get(
+            "checkpoint_every", eval_every
+        )
+        primary_metric = self.config["task"].get(
+            "primary_metric", self.evaluator.task_primary_metric[self.name]
+        )
+        backward_every = self.config["optim"].get("backward_every", 1)
+        if (
+            not hasattr(self, "primary_metric")
+            or self.primary_metric != primary_metric
+        ):
+            self.best_val_metric = 1e9 if "mae" in primary_metric else -1.0
+        else:
+            primary_metric = self.primary_metric
+        self.metrics = {}
+
+        # Calculate start_epoch from step instead of loading the epoch number
+        # to prevent inconsistencies due to different batch size in checkpoint.
+        start_epoch = self.step // len(self.train_loader)
+        count = 0
+
+        for epoch_int in range(
+            start_epoch, self.config["optim"]["max_epochs"]
+        ):
+            self.train_sampler.set_epoch(epoch_int)
+            skip_steps = self.step % len(self.train_loader)
+            train_loader_iter = iter(self.train_loader)
+
+            for i in range(skip_steps, len(self.train_loader)):
+                self.epoch = epoch_int + (i + 1) / len(self.train_loader)
+                self.step = epoch_int * len(self.train_loader) + i + 1
+                self.model.train()
+
+                # Get a batch.
+                batch = next(train_loader_iter)
+                # Forward, loss, backward every N batch (important!!!).
+                with torch.cuda.amp.autocast(enabled=self.scaler is not None):
+                    out = self._forward(batch)
+                    loss = self._compute_loss(out, batch)
+                loss = self.scaler.scale(loss) if self.scaler else loss
+                count += 1
+                # this will not be an accurate estimation of the accumulated loss
+                # but should be very close.
+                self._backward(
+                    loss / backward_every,
+                    (count == backward_every)
+                    or (
+                        i == len(self.train_loader) - 1
+                        and epoch_int == self.config["optim"]["max_epochs"] - 1
+                    ),
+                )
+                if count == backward_every:
+                    count = 0
+
+                scale = self.scaler.get_scale() if self.scaler else 1.0
+
+                # Compute metrics.
+                with torch.no_grad():
+                    self.metrics = self._compute_metrics(
+                        out,
+                        batch,
+                        self.evaluator,
+                        self.metrics,
+                    )
+                    self.metrics = self.evaluator.update(
+                        "loss", loss.item() / scale, self.metrics
+                    )
+
+                # Log metrics.
+                log_dict = {k: self.metrics[k]["metric"] for k in self.metrics}
+                log_dict.update(
+                    {
+                        "lr": self.scheduler.get_lr(),
+                        "epoch": self.epoch,
+                        "step": self.step,
+                    }
+                )
+                if (
+                    self.step % self.config["cmd"]["print_every"] == 0
+                    and distutils.is_master()
+                    and not self.is_hpo
+                ):
+                    log_str = [
+                        "{}: {:.2e}".format(k, v) for k, v in log_dict.items()
+                    ]
+                    logging.info(", ".join(log_str))
+                    self.metrics = {}
+
+                if self.logger is not None:
+                    self.logger.log(
+                        log_dict,
+                        step=self.step,
+                        split="train",
+                    )
+
+                if (
+                    checkpoint_every != -1
+                    and self.step % checkpoint_every == 0
+                ):
+                    self.save(
+                        checkpoint_file="checkpoint.pt", training_state=True
+                    )
+
+                # Evaluate on val set every `eval_every` iterations.
+                torch.cuda.empty_cache()
+                if self.step % eval_every == 0:
+                    if self.val_loader is not None:
+                        val_metrics = self.validate(
+                            split="val",
+                            disable_tqdm=disable_eval_tqdm,
+                        )
+                        self.update_best(
+                            primary_metric,
+                            val_metrics,
+                            disable_eval_tqdm=disable_eval_tqdm,
+                        )
+                        if self.is_hpo:
+                            self.hpo_update(
+                                self.epoch,
+                                self.step,
+                                self.metrics,
+                                val_metrics,
+                            )
+
+                    if self.config["task"].get("eval_relaxations", False):
+                        if "relax_dataset" not in self.config["task"]:
+                            logging.warning(
+                                "Cannot evaluate relaxations, relax_dataset not specified"
+                            )
+                        else:
+                            self.run_relaxations()
+
+                if self.scheduler.scheduler_type == "ReduceLROnPlateau":
+                    if self.step % eval_every == 0:
+                        self.scheduler.step(
+                            metrics=val_metrics[primary_metric]["metric"],
+                        )
+                else:
+                    self.scheduler.step()
+
+            torch.cuda.empty_cache()
+
+            if checkpoint_every == -1:
+                self.save(checkpoint_file="checkpoint.pt", training_state=True)
+
+        self.train_dataset.close_db()
+        if self.config.get("val_dataset", False):
+            self.val_dataset.close_db()
+        if self.config.get("test_dataset", False):
+            self.test_dataset.close_db()
+
+    def _backward(self, loss, if_update) -> None:
+        loss.backward()
+        # Scale down the gradients of shared parameters
+        if hasattr(self.model.module, "shared_parameters"):
+            for p, factor in self.model.module.shared_parameters:
+                if hasattr(p, "grad") and p.grad is not None:
+                    p.grad.detach().div_(factor)
+                else:
+                    if not hasattr(self, "warned_shared_param_no_grad"):
+                        self.warned_shared_param_no_grad = True
+                        logging.warning(
+                            "Some shared parameters do not have a gradient. "
+                            "Please check if all shared parameters are used "
+                            "and point to PyTorch parameters."
+                        )
+        if self.clip_grad_norm:
+            if self.scaler:
+                self.scaler.unscale_(self.optimizer)
+            grad_norm = torch.nn.utils.clip_grad_norm_(
+                self.model.parameters(),
+                max_norm=self.clip_grad_norm,
+            )
+            if self.logger is not None:
+                self.logger.log(
+                    {"grad_norm": grad_norm}, step=self.step, split="train"
+                )
+        if if_update:
+            if self.scaler:
+                self.scaler.step(self.optimizer)
+                self.scaler.update()
+            else:
+                self.optimizer.step()
+            if self.ema:
+                self.ema.update()
+            self.optimizer.zero_grad()
